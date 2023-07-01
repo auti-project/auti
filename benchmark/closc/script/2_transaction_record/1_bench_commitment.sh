669b8c56d70703febc27adc14b7890732b66a219
txaty <txaty@proton.me>
2023-07-01 07:01:57 +0000

    Add CLOSC TR bench scripts.

---
+++ benchmark/closc/script/2_transaction_record/1_bench_commitment.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+HOME_DIR="../.."
+cd $HOME_DIR || exit
+
+source ./script/clean_up.sh
+
+go build -o closc.out
+
+LOG_DIR="logs"
+if [ ! -d $LOG_DIR ]; then
+  mkdir $LOG_DIR
+fi
+LOG_FILE_DIR="${LOG_DIR}/closc_tr_commit.log"
+if [ -f $LOG_FILE_DIR ]; then
+  rm $LOG_FILE_DIR
+fi
+touch $LOG_FILE_DIR
+
+for i in 1000 10000 100000 1000000; do
+  for j in {1..10}; do
+    echo "No: $j" >>$LOG_FILE_DIR
+    ./closc.out -phase tr -process commitment -num $i -numIter 1 | tee -a $LOG_FILE_DIR
+    sleep 1
+  done
+done
+
+rm closc.out
